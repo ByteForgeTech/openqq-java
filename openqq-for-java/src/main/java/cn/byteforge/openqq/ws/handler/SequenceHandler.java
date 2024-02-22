5dc9e6149afd85a4ce8a06e25844e5688e49561f
IllTamer <78360471+IllTamer@users.noreply.github.com>
2024-02-22 22:29:43 +0800

    feat: add sequence handler

---
+++ openqq-for-java/src/main/java/cn/byteforge/openqq/ws/handler/SequenceHandler.java
@@ -0,0 +1,21 @@
+package cn.byteforge.openqq.ws.handler;
+
+import cn.byteforge.openqq.ws.event.Event;
+
+/**
+ * 序列号处理
+ * */
+public class SequenceHandler extends ChainHandler {
+
+    // Event -> Event
+    @Override
+    protected Object doHandle(Object o) {
+        Event event = (Event) o;
+        Long serialNumber = event.getSerialNumber();
+        if (serialNumber != null) {
+            getContext().setReceivedSerialNumber(serialNumber);
+        }
+        return o;
+    }
+
+}
