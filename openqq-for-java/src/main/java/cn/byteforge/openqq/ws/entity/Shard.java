e198cced6813fbbd41e963df934fb55765177712
IllTamer <78360471+IllTamer@users.noreply.github.com>
2024-02-22 22:28:28 +0800

    fix: error shard array

--- openqq-for-java/src/main/java/cn/byteforge/openqq/ws/entity/Shard.java
+++ openqq-for-java/src/main/java/cn/byteforge/openqq/ws/entity/Shard.java
@@ -13,15 +13,15 @@ public class Shard {
     public static final Shard STANDALONE = new Shard(0, 1);
 
     /**
-     * 分片数
-     * */
-    private Integer size;
-
-    /**
      * 当前分片索引
      * [0, size-1]
      * */
     private Integer index;
+
+    /**
+     * 分片数
+     * */
+    private Integer size;
 
     public int[] toArray() {
         return new int[] {index, size};
