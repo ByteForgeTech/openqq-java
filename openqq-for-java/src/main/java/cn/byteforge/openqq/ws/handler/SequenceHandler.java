d7939fd8b7a0741886873b7aeb7198f9ef10a197
IllTamer <78360471+IllTamer@users.noreply.github.com>
2024-02-23 22:37:30 +0800

    feat: complete sequence handler

--- openqq-for-java/src/main/java/cn/byteforge/openqq/ws/handler/SequenceHandler.java
+++ openqq-for-java/src/main/java/cn/byteforge/openqq/ws/handler/SequenceHandler.java
@@ -1,21 +1,49 @@
 package cn.byteforge.openqq.ws.handler;
 
 import cn.byteforge.openqq.ws.event.Event;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
 /**
  * 序列号处理
  * */
-public class SequenceHandler extends ChainHandler {
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class SequenceHandler {
 
-    // Event -> Event
-    @Override
-    protected Object doHandle(Object o) {
-        Event event = (Event) o;
-        Long serialNumber = event.getSerialNumber();
-        if (serialNumber != null) {
-            getContext().setReceivedSequence(serialNumber);
+    /**
+     * 收到的序列号记录
+     * */
+    public static final class Received extends ChainHandler {
+
+        // Event -> Event
+        @Override
+        protected Object doHandle(Object o) {
+            Event event = (Event) o;
+            Long sequenceNumber = event.getSequenceNumber();
+            if (sequenceNumber != null) {
+                getContext().setReceivedSequence(sequenceNumber);
+            }
+            return o;
         }
-        return o;
+
+    }
+
+    /**
+     * 完成处理的序列号记录
+     * */
+    public static final class Handled extends ChainHandler {
+
+        // Event -> Event
+        @Override
+        protected Object doHandle(Object o) {
+            Event event = (Event) o;
+            Long sequenceNumber = event.getSequenceNumber();
+            if (sequenceNumber != null) {
+                getContext().setHandledSequence(sequenceNumber);
+            }
+            return o;
+        }
+
     }
 
 }
